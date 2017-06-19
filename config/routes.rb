8e071c13d74483749ee07be38cc39286723ebe1d
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2017-06-19 10:38:23 +0800

    finish backend of simple chat app using actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+  root 'chat_rooms#index'
 end
